93494478839944acfcf6b4980a1cb7ed9bb9c272
Andi Eder <aeder@hsr.ch>
2018-05-29 11:14:35 +0200

    Added base configuration of reinforcement learning player

---
+++ sljassbot/player/rl_player.py
@@ -0,0 +1,189 @@
+import logging
+import random
+from collections import deque
+
+import numpy as np
+from keras.callbacks import ReduceLROnPlateau
+
+from pyschieber.player.base_player import BasePlayer
+from pyschieber.trumpf import Trumpf
+from pyschieber.card import from_string_to_card
+from pyschieber.rules.stich_rules import allowed_cards
+from pyschieber.player.greedy_player.trumpf_decision import choose_trumpf
+from sljassbot.player.input_handler import InputHandler, card_to_index, index_to_card, print_state, \
+    count_played_cards
+from sljassbot.player.model import build_model
+
+logger = logging.getLogger(__name__)
+
+
+class RLPlayer(BasePlayer):
+    max_points = 55
+    min_points = -55
+
+    def __init__(self, name, model_path, rounds=1):
+        super().__init__(name=name)
+        self.input_handler = InputHandler()
+        self.n_samples = 20 * rounds
+        self.memories = deque([], maxlen=2 * self.n_samples)
+        self.model = build_model(model_path=model_path)
+        self.model_t = build_model(model_path=model_path)
+        self.loss = []
+        self.epsilon = 0.95  # exploration rate
+        self.penalty = 0.
+        self.gamma = 0.95
+        self.callbacks = [ReduceLROnPlateau(monitor='loss', factor=0.2, patience=5, min_lr=0.01)]
+        self.won = 0
+        self.lost = 0
+        self.current_memory = dict(used=False)
+        self.previous_memory = dict(used=False)
+        self.team_points = [0, 0]
+        self.won_stich = [0, 0, 0, 0]
+        self.won = [0, 0, 0]
+
+    def remember(self, state, action, reward, next_state, done, hand_cards, table_cards, trumpf):
+        self.memories.append((state, action, reward, next_state, done, hand_cards, table_cards, trumpf))
+
+    def act(self, input_state):
+        if random.uniform(0, 1) >= self.epsilon:
+            random_list = [i for i in range(InputHandler.output_size)]
+            random.shuffle(random_list)
+            return np.random.random_sample(InputHandler.output_size), np.array(random_list)
+        state = np.expand_dims(input_state, axis=0)
+        act_values = self.model.predict(state)
+        return act_values, np.argsort(act_values[0])[::-1]
+
+    def replay(self):
+        for state, action, reward, next_state, done, hand_cards, table_cards, trumpf in self.get_batch():
+            state = np.expand_dims(state, axis=0)
+            next_state = np.expand_dims(next_state, axis=0)
+            target = reward
+            if not done:
+                # predictions = self.model_t.predict(next_state)
+                # value = self.replay_max_allowed_value(predictions, hand_cards, table_cards, trumpf)
+                target = reward + self.gamma * np.amax(self.model_t.predict(next_state)[0])
+                # target = reward + self.gamma * value
+            target_f = self.model.predict(state)
+            target_f[0][action] = target
+            history = self.model.fit(state, target_f, epochs=1, verbose=0, callbacks=self.callbacks)
+            self.loss += history.history['loss']
+        self.update_target()
+
+    def replay_max_allowed_value(self, predictions, hand_cards, table_cards, trumpf):
+        playable_cards = allowed_cards(hand_cards=hand_cards, table_cards=table_cards, trumpf=trumpf)
+        card = max_of_allowed_cards(predictions=predictions, playable_cards=playable_cards)
+        index = card_to_index(card=card)
+        return predictions[0][index]
+
+    def update_target(self):
+        weights = self.model.get_weights()
+        self.model_t.set_weights(weights)
+
+    def get_batch(self):
+        n_samples = min(self.n_samples, len(self.memories))
+        samples = random.sample(self.memories, n_samples)
+        return samples
+
+    def choose_trumpf(self, geschoben):
+        allowed = False
+        while not allowed:
+            trumpf, _ = choose_trumpf(cards=self.cards, geschoben=geschoben)
+            allowed = yield trumpf
+            if allowed:
+                yield None
+
+    def choose_card(self, state=None):
+        allowed = False
+        self.input_handler.update_state_choose_card(game_state=state, cards=self.cards, player_id=self.id)
+        predictions, prediction_indexes = self.act(self.input_handler.state)
+        card = self.max_of_allowed_cards(state=state, predictions=predictions)
+        self.current_memory['action'] = prediction_indexes[0]
+        self.current_memory['hand_cards'] = self.cards[:]
+        self.current_memory['table_cards'] = [from_string_to_card(entry['card']) for entry in state['table']]
+        self.current_memory['trumpf'] = Trumpf[state['trumpf']]
+        while not allowed:
+            allowed = yield card
+            if allowed:
+                yield None
+            else:
+                logger.info('not allowed card!')
+
+    def save_state(self, done):
+        if self.previous_memory['used']:
+            self.remember(state=self.previous_memory['state'], action=self.previous_memory['action'],
+                          reward=self.previous_memory['reward'], done=self.previous_memory['done'],
+                          hand_cards=self.previous_memory['hand_cards'],
+                          table_cards=self.previous_memory['table_cards'], trumpf=self.previous_memory['trumpf'],
+                          next_state=self.current_memory['state'])
+
+        self.previous_memory = self.current_memory.copy()
+        if done:
+            # print_state(self.previous_memory['state'], 0)
+            self.remember(state=self.previous_memory['state'], action=self.previous_memory['action'],
+                          reward=self.previous_memory['reward'], done=self.previous_memory['done'],
+                          hand_cards=self.previous_memory['hand_cards'],
+                          table_cards=self.previous_memory['table_cards'], trumpf=self.previous_memory['trumpf'],
+                          next_state=None)
+            self.input_handler.reset()
+            # print('Number of played cards: ',
+            #      count_played_cards(input_state=self.previous_memory['state'], player_id=self.id))
+
+    def stich_over(self, state=None):
+        done = True if len(self.cards) == 0 else False
+        last_stich = state['stiche'][-1] if state['stiche'] else None
+        self.current_memory['state'] = np.copy(self.input_handler.state)
+        self.current_memory['reward'] = self.calculate_reward(state['teams'], done=done,
+                                                              stich=last_stich) if last_stich else 0
+        self.current_memory['done'] = done
+        self.current_memory['used'] = True
+        self.save_state(done=done)
+        self.input_handler.update_state_stich(game_state=state, cards=self.cards, player_id=self.id)
+
+    def calculate_reward(self, teams, done, stich):
+        stich_player_id = stich['player_id']
+        self.won_stich[stich_player_id] += 1
+        team1 = teams[0]['points'] - self.team_points[0]
+        team2 = teams[1]['points'] - self.team_points[1]
+        self.team_points[0] = teams[0]['points']
+        self.team_points[1] = teams[1]['points']
+        if done:
+            if team1 > team2:
+                self.won[0] += 1
+                return 2.5
+            elif team1 == team2:
+                self.won[2] += 1
+                return 0
+            else:
+                self.won[1] += 1
+                return -2.5
+        if team1 > team2:
+            return self.normalize_reward(team1)
+        elif team1 == team2:
+            return 0
+        else:
+            return -self.normalize_reward(team2)
+
+    def reset_stats(self):
+        self.won = [0, 0, 0]
+        self.won_stich = [0, 0, 0, 0]
+
+    def max_of_allowed_cards(self, predictions, state):
+        allowed = self.allowed_cards(state=state)
+        return max_of_allowed_cards(predictions=predictions, playable_cards=allowed)
+
+    def normalize_reward(self, points):
+        return (points - self.min_points) / (self.max_points - self.min_points)
+
+    def denormalize_reward(self, reward):
+        return reward * (self.max_points - self.min_points) + self.min_points
+
+
+def max_of_allowed_cards(predictions, playable_cards):
+    indices_of_allowed_cards = [card_to_index(allowed_card) for allowed_card in playable_cards]
+    indices_sorted = np.argsort(predictions)[::-1]
+    for i in np.nditer(indices_sorted, order='C'):
+        try:
+            indices_of_allowed_cards.index(i)
+            return index_to_card(i)
+        except ValueError:
+            pass
---
+++ tests/player/test_reinforcement_player.py
@@ -0,0 +1,135 @@
+import os
+import inspect
+from timeit import default_timer as timer
+
+import pytest
+from pyschieber.player.greedy_player.greedy_player import GreedyPlayer
+from pyschieber.player.challenge_player.challenge_player import ChallengePlayer
+
+from pyschieber.player.random_player import RandomPlayer
+from pyschieber.tournament import Tournament
+
+from sljassbot.player.rl_player import RLPlayer
+
+
+@pytest.fixture(scope='module')
+def weights_directory():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    return dir_path + '/weights/'
+
+
+@pytest.mark.statistical  #
+def test_random_vs_random():
+    players = [RandomPlayer(name='Tick'), RandomPlayer(name='Track'),
+               RandomPlayer(name='Dagober'), RandomPlayer(name='Trick')]
+    get_function_name()
+    run_game(players)
+
+
+@pytest.mark.statistical  #
+def test_greed_vs_greedy():
+    players = [GreedyPlayer(name='Greedy1'), GreedyPlayer(name='Greedy2'),
+               GreedyPlayer(name='Greedy3'), GreedyPlayer(name='Greedy4')]
+    get_function_name()
+    run_game(players)
+
+
+@pytest.mark.statistical  #
+def test_greed_vs_random():
+    players = [GreedyPlayer(name='Greedy1'), RandomPlayer(name='Random1'),
+               GreedyPlayer(name='Greedy2'), RandomPlayer(name='Random2')]
+    get_function_name()
+    run_game(players)
+
+
+@pytest.mark.statistical
+def test_rl_vs_random(weights_directory):
+    model_path = weights_directory + 'rl1_model.h5'
+    players = [RLPlayer(name='RLPlayer1', model_path=model_path), RandomPlayer(name='Track'),
+               RLPlayer(name='RLPlayer2', model_path=model_path), RandomPlayer(name='Trick')]
+    get_function_name()
+    run_game(players)
+
+
+@pytest.mark.statistical
+def test_rl_vs_greedy(weights_directory):
+    model_path = weights_directory + 'rl1_model.h5'
+    players = [RLPlayer(name='RLPlayer1', model_path=model_path), GreedyPlayer(name='Greedy1'),
+               RLPlayer(name='RLPlayer2', model_path=model_path), GreedyPlayer(name='Greedy2')]
+    get_function_name()
+    run_game(players)
+
+
+@pytest.mark.statistical  #
+def test_greedy_vs_challenge():
+    players = [GreedyPlayer(name='GreedyPlayer1'), ChallengePlayer(name='ChallengePlayer1'),
+               GreedyPlayer(name='GreedyPlayer2'), ChallengePlayer(name='ChallengePlayer2')]
+    get_function_name()
+    run_game(players)
+
+
+@pytest.mark.statistical
+def test_rl_vs_challenge(weights_directory):
+    model_path = weights_directory + 'rl1_model.h5'
+    players = [RLPlayer(name='RLPlayer1', model_path=model_path), ChallengePlayer(name='ChallengePlayer1'),
+               RLPlayer(name='RLPlayer2', model_path=model_path), ChallengePlayer(name='ChallengePlayer2')]
+    get_function_name()
+    run_game(players)
+
+
+@pytest.mark.statistical
+def test_rl_greedy_vs_rl_challenge(weights_directory):
+    model_path = weights_directory + 'rl1_model_challenge.h5'
+    model_path_greedy = weights_directory + 'rl1_model.h5'
+    players = [RLPlayer(name='RLGreedyPlayer1', model_path=model_path_greedy),
+               RLPlayer(name='RLChallengePlayer1', model_path=model_path),
+               RLPlayer(name='RLGreedyPlayer2', model_path=model_path_greedy),
+               RLPlayer(name='RLChallengePlayer2', model_path=model_path)]
+    get_function_name()
+    run_game(players)
+
+
+@pytest.mark.statistical  #
+def test_greedy_and_challenge_vs_2_challenge():
+    players = [GreedyPlayer(name='Gready1'), ChallengePlayer(name='ChallengePlayer1'),
+               ChallengePlayer(name='ChallengePlayer2'), ChallengePlayer(name='ChallengePlayer3')]
+    get_function_name()
+    run_game(players)
+
+
+@pytest.mark.statistical  #
+def test_random_and_challenge_vs_2_challenge():
+    players = [RandomPlayer(name='Random1'), ChallengePlayer(name='ChallengePlayer1'),
+               ChallengePlayer(name='ChallengePlayer2'), ChallengePlayer(name='ChallengePlayer3')]
+    get_function_name()
+    run_game(players)
+
+
+def run_game(players, point_limit=1000, number_of_tournaments=1000):
+    tournament = Tournament(point_limit=point_limit)
+    [tournament.register_player(player=player) for player in players]
+
+    team_1_won = 0
+    team_2_won = 0
+
+    start = timer()
+
+    for _ in range(number_of_tournaments):
+        tournament.play()
+        if tournament.teams[0].won(point_limit=point_limit):
+            team_1_won += 1
+        else:
+            team_2_won += 1
+
+    end = timer()
+    print("\nTo run {0} tournaments it took {1:.2f} seconds.".format(number_of_tournaments, end - start))
+
+    difference = abs(team_1_won - team_2_won)
+    print("Difference: ", difference)
+    print("Team 1 ({0}, {1}): {2}".format(players[0].name, players[2].name, team_1_won))
+    print("Team 2 ({0}, {1}): {2}".format(players[1].name, players[3].name, team_2_won))
+    assert True
+
+
+def get_function_name():
+    print("\n \n{}".format(inspect.stack()[1][3]))
